d4aee82685fb939dd2ecaf82f6cc6e5825e1906c
Shira Zelefsky <shirazelefsky@gmail.com>
2019-08-06 10:57:22 -0400

    problem fixed thanks to connie

--- taskListMVP.js
+++ taskListMVP.js
@@ -1,10 +1,14 @@
-  var counter = 1;
+var counter = 1;
+var list = document.getElementById("taskList");
 
 function timeFunct(){
   var taskName = prompt("please enter task name");
 
   if (taskName != null){
-    document.getElementById(`listItem${counter}`).innerText = taskName;
+    let x = document.createElement("li");
+    x.innerText = taskName;
+    list.append(x);
+    // document.getElementById(`listItem${counter}`).innerText = taskName;
     counter ++;
   }
 }
