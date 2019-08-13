09f64ffbeb2a8c4db2a2f9de0c52652b1739b155
Shira Zelefsky <shirazelefsky@gmail.com>
2019-08-13 15:58:05 -0400

    finishing touches for the day

--- mvp.js
+++ mvp.js
@@ -100,7 +100,7 @@ function timeFunct() {
   if (st == "stopped") {
     if (items.length > 0) {
       var i = items.pop(0);
-      document.getElementById("currenttask").innerText = "-- Working on " + i["name"];
+      document.getElementById("currenttask").innerText = "-- Working on " + i["name"]+i["priority"]+i["name"];
       hour = i["hour"];
       minute = i["min"];
       second = i["second"];
